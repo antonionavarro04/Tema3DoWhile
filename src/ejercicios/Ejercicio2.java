1751e82b2a3c4cd195129ff5decb0ebb7fde3675
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-10-31 09:03:23 +0100

    Terminado

--- src/ejercicios/Ejercicio2.java
+++ src/ejercicios/Ejercicio2.java
@@ -1,5 +1,23 @@
 package ejercicios;
 
 public class Ejercicio2 {
-    
+    public static void main(String[] args) {
+        // ? Realiza un programa Java que muestre los numeros pares comprendidos entre el 1 y el 200
+        // ^ Creamos la variable num que se incrementara en 2 y empezará en 2
+        int num = 2;
+
+        // ! Pequeña explicación del Programa
+        System.out.println("Programa Java que muestre los numeros pares comprendidos entre el 1 y el 200");
+
+        // ? Salto de Linea Triple
+        System.out.println();System.out.println("-------------------------------");System.out.println();
+
+        // ! Creamos el Bucle Do-While
+        do{
+            System.out.println("-> " + num);
+            num += 2;
+        } while (num <= 200);
+
+        // € Hecho por Antonio Navarro
+    }
 }
