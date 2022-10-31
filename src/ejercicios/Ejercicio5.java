9253f9ba92ee3acae3b6a7b80b908b5fdb572d08
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-10-31 10:18:40 +0100

    Terminado, falta revision de Comentarios

--- src/ejercicios/Ejercicio5.java
+++ src/ejercicios/Ejercicio5.java
@@ -3,16 +3,14 @@ package ejercicios;
 // ? Importamos la clase Scanner y Locale
 import java.util.Scanner;
 import java.util.Locale;
-import java.util.Random;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
         // ? Programa que el usuario piensa un numero entre el 1 al 100 y el programa lo adivina, el usuario le dira si el numero es mayor o menor que el numero introducido
-        // ^ Creamos la variable rng
-        Random rng = new Random();
+        // ^ Creamos las variables num, mayor y menor
+        int num, mayor = 100, menor = 1;
 
-        // ^ Creamos la variable menor y mayor
-        int menor = 1, mayor = 100;
+        num = (int) (Math.random() * (100 - 1)) + 1;
 
         // ^ Creamos las variables final menor, mayor o igual
         final int MAYOR = 1;
@@ -25,35 +23,36 @@ public class Ejercicio5 {
         // ^ Creamos la variable boolean haAdivinado
         boolean haAdivinado = false;
 
-        // ! Generamos un numero int entre 1 y 100
-        int num = rng.nextInt(0, 100);
-
         // ^ Creamos el Scanner en la variable read y cambiamos al formato de US
         Scanner read = new Scanner(System.in);
         read.useLocale(Locale.US);
 
         // ! Bucle Do-While
         do{
-            System.out.println("Es " + num + "tu número?");
-            System.out.println("[1] Mayor al numero que estas pensando");
-            System.out.println("[2] Menor al numero que estas pensando");
-            System.out.println("[3] Igual al numero que estas pensando");
+            System.out.println("Es " + num + ":");
+            System.out.println("[1] Mayor al numero que estas pensando?");
+            System.out.println("[2] Menor al numero que estas pensando?");
+            System.out.println("[3] Igual al numero que estas pensando?");
+            System.out.print("-> ");
             userChoice = read.nextByte();
+
+            // ? Salto de Linea Triple
+            System.out.println();System.out.println("-------------------------------");System.out.println();
 
             if (userChoice == MAYOR){
                 mayor = num;
-                rng.nextInt(menor, mayor);
+                num = (int) (Math.random() * (mayor - menor)) + menor;
             } else if (userChoice == MENOR){
                 menor = num;
-                rng.nextInt(menor, mayor);
+                num = (int) (Math.random() * (mayor - menor)) + menor;
             } else if (userChoice == IGUAL){
                 haAdivinado = true;
-                System.out.println("Adivine tu Número");
+                System.out.println("Adiviné tu Número");
             } else{
                 haAdivinado = true;
                 System.out.println("Se ha producido un error");
             }
-        } while (haAdivinado);
+        } while (haAdivinado == false);
 
         // ^ Cerramos el Scanner
         read.close();
