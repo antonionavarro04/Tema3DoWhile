63a68e129354a9e2988c4340a4d66aa059fcae96
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-02 13:31:14 +0100

    V. Final 2.0 // Contador

--- src/ejercicios/Ejercicio5.java
+++ src/ejercicios/Ejercicio5.java
@@ -14,12 +14,13 @@ public class Ejercicio5 {
         num = (int) (Math.random() * (100 - 1)) + 1;
 
         // ^ Creamos las variables finales, MAYOR, MENOR e IGUAL para ayudarnos a comparar los numeros
-        final int MAYOR = 1;
-        final int MENOR = 2;
-        final int IGUAL = 3;
+        final byte MAYOR = 1;
+        final byte MENOR = 2;
+        final byte IGUAL = 3;
 
-        // ^ Creamos la variable userChoice para guardar la eleccion del usuario
+        // ^ Creamos la variable userChoice para guardar la eleccion del usuario y intentos para guardar el numero de intentos
         byte userChoice = 0;
+        byte intentos = 0;
 
         // ^ Creamos la variable boolean haAdivinado, empezará siendo false y será true cuando el usuario diga que el numero es igual
         boolean haAdivinado = false;
@@ -30,30 +31,43 @@ public class Ejercicio5 {
 
         // ! Bucle Do-While
         do{
-            System.out.println("Es " + num + ":");
-            System.out.println("[1] Mayor al numero que estas pensando?");
-            System.out.println("[2] Menor al numero que estas pensando?");
-            System.out.println("[3] Igual al numero que estas pensando?");
+            // ! Incrementamos los intentos en 1
+            intentos++;
+
+            // ! Mostramos el numero y le preguntamos al usuario si es mayor, menor o igual
+            System.out.println(num + " es:");
+            System.out.println("[1] Mayor a mi numero");
+            System.out.println("[2] Menor a mi numero");
+            System.out.println("[3] Igual a mi numero");
             System.out.print("-> ");
             userChoice = read.nextByte();
 
             // ? Salto de Linea Triple
             System.out.println();System.out.println("-------------------------------");System.out.println();
 
-            if (userChoice == MAYOR){
+            // ! Comprobamos que no ha superado 100 intentos
+            // ? Funcionalidad extra que yo le he añadido al programa ya que es raro que no adivine el numero en 100 intentos, la media suele ser entre 5 y 10, como mucho 15
+            if(intentos == 100){ // * En caso de que se hayan hecho 100 intentos
+                haAdivinado = true;
+                System.out.println("No he podido adivinar tu numero en menos de 100 intentos");
+            }
+
+            // ! Comprobamos que el numero introducido por el usuario sea mayor, menor o igual
+            if (userChoice == MAYOR){ // * En caso de que sea mayor
                 mayor = num;
                 num = (int) (Math.random() * (mayor - menor)) + menor;
-            } else if (userChoice == MENOR){
+            } else if (userChoice == MENOR){ // * En caso de que sea menor
                 menor = num;
                 num = (int) (Math.random() * (mayor - menor)) + menor;
-            } else if (userChoice == IGUAL){
+            } else if (userChoice == IGUAL){ // * En caso de que sea igual
                 haAdivinado = true;
                 System.out.println("Adiviné tu Número");
-            } else{
+                System.out.println("Intentos: " + intentos);
+            } else{ // * En caso de que el usuario introduzca un numero que no sea 1, 2 o 3
                 haAdivinado = true;
                 System.out.println("Se ha producido un error");
             }
-        } while (haAdivinado == false);
+        } while (haAdivinado == false); // * Mientras que haAdivinado sea 'false' el programa seguira ejecutando el bucle
 
         // ^ Cerramos el Scanner
         read.close();
